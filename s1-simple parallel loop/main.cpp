4a1830b89ec51d49b7893a6cc4fd10d29643cdfc
pengxianjun <pengxianjun@misrobot.com>
2017-02-08 15:15:32 +0800

    modify s1

--- s1-simple parallel loop/main.cpp
+++ s1-simple parallel loop/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <omp.h>
+#include <windows.h>
 
-#define ARRAY_SIZE 100
+#define ARRAY_SIZE 1000
 
 using namespace std;
 
@@ -10,7 +11,7 @@ int main()
 	int threadIds[ARRAY_SIZE] = { 0 };
 
 #pragma omp parallel for
-	for(int i = 0; i < 100; ++i)
+	for(int i = 0; i < ARRAY_SIZE; ++i)
 	{
 		//这里循环迭代器是一个私有变量，所有不需要用private子句明显指明
 		threadIds[i] = omp_get_thread_num();
