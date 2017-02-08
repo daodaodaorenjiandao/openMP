264cca31f3b0923e67d9a8c01f215670f076b085
pengxianjun <pengxianjun@misrobot.com>
2017-02-08 13:41:42 +0800

    s1:a simple parallel loop

---
+++ s1-simple parallel loop/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <omp.h>
+
+#define ARRAY_SIZE 100
+
+using namespace std;
+
+int main()
+{
+	int threadIds[ARRAY_SIZE] = { 0 };
+
+#pragma omp parallel for
+	for(int i = 0; i < 100; ++i)
+	{
+		//这里循环迭代器是一个私有变量，所有不需要用private子句明显指明
+		threadIds[i] = omp_get_thread_num();
+	}
+
+	cout << "thread id : \n";
+	for(auto e : threadIds)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+
+	cin.get();
+	return 0;
+}
